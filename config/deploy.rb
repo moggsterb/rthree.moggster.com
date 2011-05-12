4b548b91d9da54b036022fb9f30bb2c5ac12e718
moggsterb <moggster@moggster.com>
2011-05-12 07:55:48 +0100

    Initial Import

---
+++ config/deploy.rb
@@ -0,0 +1,36 @@
+require 'bundler/capistrano'
+
+default_run_options[:pty] = false
+ssh_options[:forward_agent] = true
+set :use_sudo, false
+set :user, "moggsterrthree"
+
+set :application, "rthree.moggster.com"
+set :repository,  "git@github.com:claco/rthree.moggster.com.git"
+set :scm, :git
+set :branch, 'master'
+set :git_shallow_clone, 1
+set :deploy_via, :remote_cache
+set :copy_compression, :bz2
+set :rails_env, 'production'
+set :deploy_to, "/home/moggsterrthree/#{application}"
+
+role :web, "#{application}"                          # Your HTTP server, Apache/etc
+role :app, "#{application}"                          # This may be the same as your `Web` server
+role :db,  "#{application}", :primary => true # This is where Rails migrations will run
+# role :db,  "your slave db-server here"
+
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
+
+
+
+
+
+
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RthreeMoggsterCom::Application.config.secret_token = '6964d379c3ae6f4a8faae8be5bf058f14e23038b57fd7070e9fecc48939f3946a73322c0503b38d18b9b3b2b4a0590a81159e55a03e0864d5998959cb3015541'
